a07c9fa89dfd3aa9c615e04f0d172a033a43d83e
Paolo Debuque <pdebuque@gmail.com>
2022-12-10 16:24:39 -0600

    front-page styling

---
+++ src/components/MovieListItem/MovieListItem.jsx
@@ -0,0 +1,47 @@
+import { useHistory } from 'react-router-dom';
+import { Paper, Typography, Box } from '@mui/material'
+import './MovieListItem.css';
+import { useState } from 'react'
+
+export default function MovieListItem({ movie }) {
+    const history = useHistory();
+    const [textShow, setText] = useState(false);
+
+    const paperStyle = {
+        backgroundImage: `url(${movie.poster})`,
+        height: 300,
+        width: 200,
+    }
+
+    return (
+        <Box
+            sx={{ marginY: 1, marginX: .5 }}
+            key={movie.id}
+            onClick={() => history.push(`/details/${movie.title}`)}
+            onMouseEnter={() => setText(true)}
+            onMouseLeave={() => setText(false)}
+        >
+            <Paper
+                className="poster-container"
+                elevation={3}
+                sx={paperStyle}
+            >
+                <Box
+                    className={textShow ? 'details-container' : 'hidden'}
+                    
+                >
+                    <Typography
+                        sx={{ color: 'white' }}
+                        className='details-text'
+                        variant='h4'
+                    >
+                        {movie.title}
+                    </Typography>
+                    <Typography variant = 'body2'>
+                        click to see more
+                    </Typography>
+                </Box>
+            </Paper>
+        </Box>
+    )
+}
