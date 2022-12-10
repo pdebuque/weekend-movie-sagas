adcced71ec6f4b24e2744d349612946bb9540009
Paolo Debuque <pdebuque@gmail.com>
2022-12-09 21:43:38 -0600

    implemented form modal

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -30,16 +30,20 @@ The Add Movie page should have the buttons:
 */
 
 import { useState } from 'react';
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Button, Box, Select, FormControl, InputLabel, MenuItem, TextField} from '@mui/material';
 
-export default function MovieForm() {
+export default function MovieForm({ setOpen }) {
 
+    const history = useHistory();
     const dispatch = useDispatch();
 
     const addMovie = (e) => {
         e.preventDefault();
         // console.log('in addMovie')
-        dispatch({ type: 'ADD_MOVIE', payload: newMovie })
+        dispatch({ type: 'ADD_MOVIE', payload: newMovie });
+        history.push('/');
     }
 
     const [newMovie, setMovie] = useState({
@@ -49,64 +53,97 @@ export default function MovieForm() {
         genre_id: ''
     })
 
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+        textAlign: 'center'
+    };
+
     return (
-        <main>
+        <Box sx={style}>
             {/* {JSON.stringify(newMovie)} */}
             <h2>add new movie</h2>
             <form onSubmit={addMovie}>
-                <div>
-                    <label htmlFor="title-input">movie title</label>
-                    <input
+                <Box sx={{marginY:1}}>
+                    <TextField
+                        size='small'
+                        label='title'
                         type="text"
                         name="title-input"
                         id="title-input"
                         value={newMovie.title}
                         onChange={(e) => setMovie({ ...newMovie, title: e.target.value })}
                     />
-                </div>
-                <div>
-                    <label htmlFor="poster-input">poster url</label>
-                    <input
+                </Box>
+                <Box sx={{marginY:1}}>
+                    <TextField
+                        size='small'
+                        label='poster url'
                         type="text"
                         name="poster-input"
                         id="poster-input"
                         value={newMovie.poster}
                         onChange={(e) => setMovie({ ...newMovie, poster: e.target.value })}
                     />
-                </div>
-                <div>
-                    <label htmlFor="description-input">movie description</label>
-                    <textarea
+                </Box>
+                <Box sx={{marginY: 1}}>
+                    <TextField
                         name="description-input"
                         id="description-input"
+                        size='small'
+                        multiline
+                        label='description'
+                        rows={2}
                         value={newMovie.description}
                         onChange={(e) => setMovie({ ...newMovie, description: e.target.value })}
                     />
-                </div>
-                <select
-                    name='genre'
-                    id='genre-input'
-                    value={newMovie.genre_id}
-                    onChange={(e) => setMovie({ ...newMovie, genre_id: e.target.value })}
-                >
-                    <option value=''>--select a genre --</option>
-                    <option value='1'>Adventure</option>
-                    <option value='2'>Animated</option>
-                    <option value='3'>Biographical</option>
-                    <option value='4'>Comedy</option>
-                    <option value='5'>Disaster</option>
-                    <option value='6'>Drama</option>
-                    <option value='7'>Epic</option>
-                    <option value='8'>Fantasy</option>
-                    <option value='9'>Musical</option>
-                    <option value='10'>Romantic</option>
-                    <option value='11'>Science Fiction</option>
-                    <option value='12'>Space-Opera</option>
-                    <option value='13'>Superhero</option>
-                </select>
-
-                <button type='submit'>add</button>
+                </Box>
+                <Box sx={{ width: 150, marginX: 'auto', marginY: 1 }}>
+                    <FormControl fullWidth>
+                        <InputLabel
+                            size='small' id='genre-input-label'>genre</InputLabel>
+                        <Select
+                            size='small'
+                            labelId='genre-input-label'
+                            label='genre'
+                            id='genre-input'
+                            value={newMovie.genre_id}
+                            onChange={(e) => setMovie({ ...newMovie, genre_id: e.target.value })}
+                        >
+                            <MenuItem value=''>--select a genre --</MenuItem>
+                            <MenuItem value={1}>Adventure</MenuItem>
+                            <MenuItem value={2}>Animated</MenuItem>
+                            <MenuItem value={3}>Biographical</MenuItem>
+                            <MenuItem value={4}>Comedy</MenuItem>
+                            <MenuItem value={5}>Disaster</MenuItem>
+                            <MenuItem value={6}>Drama</MenuItem>
+                            <MenuItem value={7}>Epic</MenuItem>
+                            <MenuItem value={8}>Fantasy</MenuItem>
+                            <MenuItem value={9}>Musical</MenuItem>
+                            <MenuItem value={10}>Romantic</MenuItem>
+                            <MenuItem value={11}>Science Fiction</MenuItem>
+                            <MenuItem value={12}>Space-Opera</MenuItem>
+                            <MenuItem value={13}>Superhero</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Button 
+                    sx={{marginX:.5, color: 'grey.900'}}
+                    size='small'
+                    onClick={() => setOpen(false)}
+                    >cancel</Button>
+                <Button
+                    sx={{marginX:.5, color: 'grey.900'}}
+                    size='small'
+                    type='submit'>add</Button>
             </form>
-        </main>
+        </Box>
     )
 }
