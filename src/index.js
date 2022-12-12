0ce8d28d1c48b4b5d491147183eb5309e45a2bf4
Paolo Debuque <pdebuque@gmail.com>
2022-12-11 20:43:45 -0600

    moved sagas and reducers out of index

--- src/index.js
+++ src/index.js
@@ -8,86 +8,11 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects';
-import axios from 'axios';
-
-// Create the rootSaga generator function
-function* rootSaga() {
-    yield takeEvery('FETCH_MOVIES', fetchAllMovies);
-    yield takeEvery('GET_GENRES', getGenres);
-    yield takeEvery('ADD_MOVIE', addMovie);
-    yield takeEvery('EDIT_MOVIE', editMovie)
-}
-
-function* fetchAllMovies() {
-    // get all movies from the DB
-    try {
-        const movies = yield axios.get('/api/movie');
-        console.log('get all:', movies.data);
-        yield put({ type: 'SET_MOVIES', payload: movies.data });
-
-    } catch {
-        console.log('get all error');
-    }
-
-}
-
-function* getGenres(action) {
-    try {
-        // console.log('getting genres')
-        const genres = yield axios.get(`/api/genre/${action.payload}`);
-        // console.log('got genres: ', genres.data)
-        yield put({ type: 'SET_GENRES', payload: genres.data })
-    } catch (err) {
-        console.log('get failed')
-    }
-}
-
-function* addMovie(action) {
-    try {
-        console.log('adding movie: ', action.payload)
-        // need to add movie to the movie table and the genre to the genre database
-        yield axios.post('/api/movie', action.payload);
-
-        // to add genre, need the movie id. genre id is in the action payload
-    } catch (err) {
-        console.log('could not add', err)
-    }
-}
-
-function* editMovie(action) {
-    try{
-        console.log('received action:', action.payload)
-        yield axios.put(`/api/movie/${action.payload.id}`, action.payload)
-        console.log('successfully edited data')
-        yield put({type: 'FETCH_MOVIES'})
-
-    } catch(err){console.log('could not edit', err)}
-}
+import rootSaga from './redux/sagas'
+import {movies, genres} from './redux/reducers'
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
-
-// Used to store movies returned from the server
-const movies = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_MOVIES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
-
-// Used to store the movie genres
-const genres = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_GENRES':
-            // console.log('setting the genres')
-            return action.payload;
-        default:
-            return state;
-    }
-}
 
 // Create one store that all components can use
 const storeInstance = createStore(
